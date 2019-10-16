7177075c9b7bddb863521d35e93727cb0fbb4863
Jonathan de Flaugergues <jonathan.deflaugergues@abbeal.com>
2019-10-16 10:31:46 +0200

    doc: update doc

--- server/api/index.js
+++ server/api/index.js
@@ -16,10 +16,15 @@ router
   .use('/authors', authors.routes(), authors.allowedMethods())
   .get('/', async (ctx) => {
     ctx.type = 'application/hal+json';
-    ctx.body = halson()
+    ctx.body = halson({
+      welcome: 'Welcome to HAL explorer demo server',
+      hint_1: 'Click the book icon to read docs for the link relation',
+      hint_2: 'Click the blue buttons on the right to follow a link with a GET request',
+      hint_3: 'Click the green buttons on the right to make a POST request'
+    })
       .addLink('self', `${mountPoint}/`)
       .addLink('curies', [{ name: 'hal', href: `${mountPoint}/docs/resources/{rel}.md`, templated: true }])
-      .addLink('hal:authors', { href: `${mountPoint}/authors`, name: 'test', title: 'Authors' })
+      .addLink('hal:authors', { href: `${mountPoint}/authors`, title: 'Authors' })
   });
 
 module.exports = router;
