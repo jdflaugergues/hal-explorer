a53328804d81ce3a98dae97b4e4379afeab939a2
Jonathan de Flaugergues <jonathan.deflaugergues@abbeal.com>
2019-11-03 22:32:24 +0100

    feat: stylized http status code

--- src/components/HalInspector.vue
+++ src/components/HalInspector.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="inspector">
     <h1>Inspector</h1>
+    <hal-status></hal-status>
     <hal-headers></hal-headers>
     <hal-body></hal-body>
   </div>
 </template>
 
 <script>
+import HalStatus from './HalStatus';
 import HalHeaders from './HalHeaders';
 import HalBody from './HalBody';
 
 export default {
   name: 'HalInspector',
   components: {
+    HalStatus,
     HalHeaders,
     HalBody
   }
---
+++ src/components/HalStatus.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="status" :style="statusStyle">
+    {{ responseStatus.status }} {{ responseStatus.statusText }}
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+const mapStatus = {
+  1: { color: '#448aff' },
+  2: { color: '#5bb75b' },
+  3: { color: '#faa732' },
+  4: { color: '#ff5252' },
+  5: { color: '#ff5252' }
+};
+
+export default {
+  name: 'HalStatus',
+  computed: {
+    ...mapGetters(['responseStatus']),
+    statusStyle() {
+      if (!this.responseStatus.status) {
+        return;
+      }
+      return {
+        color: `${mapStatus[String(this.responseStatus.status)[0]].color} !important`
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+.status {
+  font-size: large;
+  font-weight: bold;
+  width: 100%;
+}
+</style>
